177f3019fe98f889315d02180b364167b633c1ae
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-23 13:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let element=document.getElementById("evaluatedText");
+let output=document.getElementById("letterCount")
+
+element.addEventListener("input",fun)
+
+function fun()
+{
+let len=element.value.length;
+output.innerText=len;
+}
